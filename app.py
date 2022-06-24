5a91c1b30d056c74d477bf15a7fe7ccbf79076c5
Vipul Jain <VJain@livegage.ai>
2022-06-24 23:23:55 +0530

    updated

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from security import authenticate,identity
 from resources.user import UserRegister
 from resources.item import Item,ItemList
 from resources.store import Store, StoreList
+from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.db'
@@ -21,8 +22,7 @@ api.add_resource(ItemList,'/items')
 api.add_resource(StoreList,'/stores')
 api.add_resource(UserRegister,'/register')
 
+db.init_app(app)
 if __name__=='__main__':
-    from db import db
-    db.init_app(app)
     app.run(port=5000,debug=True)
 
