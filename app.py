421681f737b87df4e39fc7c3f1b794baa32bad51
Vipul Jain <VJain@livegage.ai>
2022-06-24 20:04:23 +0530

    update

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask_jwt import JWT
 from security import authenticate,identity
 from resources.user import UserRegister
 from resources.item import Item,ItemList
-from db import db
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
@@ -23,6 +22,7 @@ api.add_resource(StoreList,'/stores')
 api.add_resource(UserRegister,'/register')
 
 if __name__=='__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5000,debug=True)
 
