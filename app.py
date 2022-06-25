e3bb28c6f79a5b397223ca24ba7368f66eccb5f1
Vipul Jain <VJain@livegage.ai>
2022-06-25 12:34:17 +0530

    finally updated

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -8,7 +9,8 @@ from resources.item import Item,ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.db'
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.secret_key='jose'
 api=Api(app)
@@ -21,6 +23,7 @@ api.add_resource(ItemList,'/items')
 api.add_resource(StoreList,'/stores')
 api.add_resource(UserRegister,'/register')
 
+
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
@@ -30,5 +33,5 @@ if __name__ == '__main__':
         def create_tables():
             db.create_all()
 
-    app.run(port=5000,debug=True)
+    app.run(port=5000)
 
