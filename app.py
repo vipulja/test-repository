765641bf3b3e6b21aa3351641c378c600f0a9659
Vipul Jain <VJain@livegage.ai>
2022-06-27 12:51:24 +0530

    done

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.secret_key='jose'
