4a6c9784e8be1f51b0581bf54eb6c2ee567736f2
Vipul Jain <VJain@livegage.ai>
2022-06-27 12:57:57 +0530

    done

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.config['DEBUG'] = True
 uri = os.getenv("DATABASE_URL")
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('uri', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.secret_key='jose'
 api=Api(app)
